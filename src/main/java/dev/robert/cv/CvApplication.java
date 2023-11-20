7dc298982391a5549b60b1c87eda1ab7ba6e11c1
Spiridon Robert <robert_spiri_v2@yahoo.com>
2023-11-20 10:37:30 +0200

    first commit

---
+++ src/main/java/dev/robert/cv/CvApplication.java
@@ -0,0 +1,15 @@
+package dev.robert.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvApplication.class, args);
+	}
+
+}
